5ccf38bd65d696c48b5aef67dc11835fe6ae39e6
David Eliahu <daeliahu@microsoft.com>
2018-12-05 17:33:37 -0500

    added project page

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
